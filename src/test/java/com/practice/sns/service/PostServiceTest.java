87ebe8f57785067ff5e9ac5dcd7e9f137440d86b
wndgndi <wndgn456@gmail.com>
2023-06-10 14:40:55 +0900

    Post 테스트 코드 작성

---
+++ src/test/java/com/practice/sns/service/PostServiceTest.java
@@ -0,0 +1,59 @@
+package com.practice.sns.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.practice.sns.exception.ErrorCode;
+import com.practice.sns.exception.SnsApplicationException;
+import com.practice.sns.model.entity.PostEntity;
+import com.practice.sns.model.entity.UserEntity;
+import com.practice.sns.repository.PostEntityRepository;
+import com.practice.sns.repository.UserEntityRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @MockBean
+    private PostEntityRepository postEntityRepository;
+
+    @MockBean
+    private UserEntityRepository userEntityRepository;
+
+    @Test
+    void 포스트작성이_성공한경우() {
+        String title = "title";
+        String body = "body";
+        String userName = "userName";
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        Assertions.assertDoesNotThrow(() -> postService.create(title, body, userName));
+    }
+
+    @Test
+    void 포스트작성시_요청한유저가_존재하지않는경우() {
+        String title = "title";
+        String body = "body";
+        String userName = "userName";
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () -> postService.create(title, body, userName));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, e.getErrorCode());
+
+    }
+}
