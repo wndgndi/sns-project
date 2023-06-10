d89d0f75fb6057d6220362c44cec79db4970c60d
wndgndi <wndgn456@gmail.com>
2023-06-10 14:43:20 +0900

    feat: Post Repository 생성

---
+++ src/main/java/com/practice/sns/repository/PostEntityRepository.java
@@ -0,0 +1,10 @@
+package com.practice.sns.repository;
+
+import com.practice.sns.model.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+
+}
