264468c2f45a452beed45f57c9eeae9a2c287c19
wndgndi <wndgn456@gmail.com>
2023-06-10 14:44:23 +0900

    ErrorCode와 Response 추가

--- src/main/java/com/practice/sns/exception/ErrorCode.java
+++ src/main/java/com/practice/sns/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "user name is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "password is invalid"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
     private HttpStatus status;
